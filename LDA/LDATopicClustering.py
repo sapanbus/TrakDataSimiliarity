29600f254328dd08cd07abbebcf20e95456ddd5d
Sapan Bandi <sansap123@gmail.com>
2017-06-27 13:40:10 -0700

    Initial commit for Trpkdata Similarity LDA machine learning

---
+++ LDA/LDATopicClustering.py
@@ -0,0 +1,18 @@
+'''
+Created on Jun 27, 2017
+
+@author: bandiboss
+'''
+
+def add(a,b):
+    return a+b
+
+def addFixedValue(a):
+    y = 5
+    return y +a
+
+if __name__ == '__main__':
+    print add(1,2)
+    print addFixedValue(1)
+    
+    
---
+++ SimiliarityScore/ComputeSimiliarScore.py
@@ -0,0 +1,17 @@
+'''
+Created on Jun 27, 2017
+
+@author: bandiboss
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
